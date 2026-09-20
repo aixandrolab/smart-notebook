42c542dc8d31044a9ad7a91e1ae4509a2e6470ac
Alexander Suvorov <aixandrolab@gmail.com>
2026-09-20 14:31:12 +0700

    feat: add repository's links on "About" page.

--- app/src/main/java/com/example/smart_notebook/AboutActivity.kt
+++ app/src/main/java/com/example/smart_notebook/AboutActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 
 class AboutActivity : AppCompatActivity() {
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +21,45 @@ class AboutActivity : AppCompatActivity() {
         val backButton = findViewById<ImageView>(R.id.backButton)
         val logoImage = findViewById<ImageView>(R.id.logoImage)
         val versionText = findViewById<TextView>(R.id.versionText)
+
         val githubCard = findViewById<CardView>(R.id.githubCard)
         val siteCard = findViewById<CardView>(R.id.smartLegionLabCard)
 
-        logoImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_launcher_foreground))
+        val repositoryCard = findViewById<CardView>(R.id.repositoryCard)
+        val issuesCard = findViewById<CardView>(R.id.issuesCard)
+        val releasesCard = findViewById<CardView>(R.id.releasesCard)
+
+        logoImage.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.ic_launcher_foreground)
+        )
 
         val packageInfo = packageManager.getPackageInfo(packageName, 0)
         versionText.text = "Version ${packageInfo.versionName}"
 
-        backButton.setOnClickListener {
-            finish()
-        }
+        backButton.setOnClickListener { finish() }
 
         githubCard.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, "https://github.com/aixandrolab".toUri())
-            startActivity(intent)
+            openUrl("https://github.com/aixandrolab")
         }
+
         siteCard.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, "https://smartlegionlab.com".toUri())
-            startActivity(intent)
+            openUrl("https://smartlegionlab.com")
         }
+
+        repositoryCard.setOnClickListener {
+            openUrl("https://github.com/aixandrolab/smart-notebook")
+        }
+
+        issuesCard.setOnClickListener {
+            openUrl("https://github.com/aixandrolab/smart-notebook/issues")
+        }
+
+        releasesCard.setOnClickListener {
+            openUrl("https://github.com/aixandrolab/smart-notebook/releases")
+        }
+    }
+
+    private fun openUrl(url: String) {
+        startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
     }
 }
